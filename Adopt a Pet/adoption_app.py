426b8939af02207e49121254461dd42471d44eb3
Michael Milroy <mdmilroy@gmail.com>
2022-09-25 14:54:08 -0400

    Add files via upload

---
+++ Adopt a Pet/adoption_app.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from helper import pets
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return '''
+  <h1>Adopt a Pet!</h1>
+  <p>Browse through the links below to find your new furry friend:</p>
+  <ul>
+    <li><a href="/animals/dogs">Dogs</a></li>
+    <li><a href="/animals/cats">Cats</a></li>
+    <li><a href="/animals/rabbits">Rabbits</a></li>
+  </ul>    
+  '''
+
+@app.route('/animals/<pet_type>')
+def animals(pet_type):
+  html = f'''
+    <h1>List of {pet_type}</h1>
+    <ul>
+    '''
+  for key, value in enumerate(pets[pet_type]):
+      html += f'''
+      <li><a href="/animals/{pet_type}/{key}">
+      {value["name"]}</a></li>
+      '''
+  html += '</ul>'
+  return html
+
+@app.route('/animals/<pet_type>/<int:pet_id>')
+def pet(pet_type,pet_id):
+  pet = pets[pet_type][pet_id]
+  return f'''
+    <h1>Name: {str(pet['name'])}</h1>
+    <img src='{pet['url']}'/>
+    <p>Description: {str(pet['description'])}</p>
+    <ul>
+      <li>Breed: {str(pet['breed'])}</li>
+      <li>Age: {str(pet['age'])}</li>
+    </ul>
+    '''
+
+
+
+
+
+
+
+
+
+
